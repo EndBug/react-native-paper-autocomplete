663893e801f79ad2c9472e40a1870205f83e348d
Federico Grandi <fgrandi30@gmail.com>
2020-08-17 23:38:24 +0200

    feat: add core code

---
+++ src/index.tsx
@@ -0,0 +1,95 @@
+import React, { useState } from 'react'
+import { View, StyleSheet, Platform, ScrollView, TouchableOpacity } from 'react-native'
+import { TextInput, Text } from 'react-native-paper'
+
+export interface AutocompleteProps {
+  array: string[]
+  initialValue?: string
+  label?: string
+  onValueSelected?: (value: string) => void
+  onError?: () => void
+}
+
+export default function Autocomplete({ array, initialValue, label, onValueSelected, onError }: AutocompleteProps) {
+  const [text, setText] = useState(initialValue || '')
+  const [selected, setSelected] = useState(initialValue || '')
+  const [error, setError] = useState(true)
+
+  const onFocus = () => {
+    if (text == selected && error) setError(false)
+    else if (text != selected && !error) {
+      (onError || defaultCallback)()
+      setError(true)
+    }
+  }
+
+  const showList = () => (
+    <View style={styles.listView}>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        keyboardShouldPersistTaps="handled"
+        style={styles.list}
+      >
+        {
+          array.filter(str => str.toLowerCase().includes(text.toLowerCase())).map((str, index) => (
+            <TouchableOpacity
+              accessible={true}
+              accessibilityLabel="Autocomplete list"
+              style={styles.onTop}
+              key={index}
+              onPress={
+                () => {
+                  (onValueSelected || defaultCallback)(str)
+                  setSelected(str)
+                  setText(str)
+                  setError(false)
+                }
+              }
+            >
+              <Text style={styles.autoCompleteText}>{str}</Text>
+            </TouchableOpacity>
+          ))
+        }
+      </ScrollView>
+    </View>
+  )
+
+  return (
+    <View>
+      <TextInput
+        label={label}
+        onFocus={onFocus}
+        onKeyPress={onFocus}
+        value={text}
+        error={error}
+        onChangeText={setText}
+      />
+      {error && showList()}
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  listView: {
+    position: Platform.OS == 'ios' ? 'absolute' : 'relative',
+    zIndex: Infinity,
+    width: '100%',
+    top: 0
+  },
+  list: {
+    backgroundColor: '#cecece',
+    zIndex: Infinity
+  },
+  onTop: {
+    zIndex: Infinity
+  },
+  autoCompleteText: {
+    zIndex: Infinity,
+    flex: 1,
+    padding: 10,
+    fontSize: 17,
+    fontWeight: 'bold'
+  }
+})
+
+const defaultCallback = () => { }
