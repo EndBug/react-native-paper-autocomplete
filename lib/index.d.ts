6209cce6888599df0a669ad1926ddda309ce8792
Federico Grandi <fgrandi30@gmail.com>
2020-08-18 13:46:28 +0000

    [auto] build: update build

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,9 +1,11 @@
 /// <reference types="react" />
+import { TextInputProps } from 'react-native-paper/lib/typescript/src/components/TextInput/TextInput';
 export interface AutocompleteProps {
     array: string[];
-    initialValue?: string;
+    placeholder?: string;
     label?: string;
+    textInputProps?: TextInputProps;
     onValueSelected?: (value: string) => void;
     onError?: () => void;
 }
-export default function Autocomplete({ array, initialValue, label, onValueSelected, onError }: AutocompleteProps): JSX.Element;
+export default function Autocomplete({ array, placeholder, label, textInputProps, onValueSelected, onError }: AutocompleteProps): JSX.Element;
